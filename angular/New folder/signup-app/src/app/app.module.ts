179a25734e01862a6d23707bcdbd2166ef32c6ed
Tran203 <tshetshe203@gmail.com>
2023-08-10 16:00:45 +0200

    update

--- angular/New folder/signup-app/src/app/app.module.ts
+++ angular/New folder/signup-app/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import this line
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './signup/signup.component'; // Make sure you import your SignupComponent if not done already
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SignupComponent],
+  imports: [BrowserModule, FormsModule], // Add FormsModule here
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular/New folder/signup-app/src/app/signup/signup.component.ts
+++ angular/New folder/signup-app/src/app/signup/signup.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  formData = { email: '', password: '' };
 
+  onSubmit() {
+    // Access the form data here
+    console.log('Form data submitted:', this.formData);
+
+    // You can now process the form data or send it to your app's backend
+    // For demonstration purposes, let's just log it to the console.
+  }
 }
+
