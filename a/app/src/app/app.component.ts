b3470667129cd69bcddc19ed4e4f7cdc74934da9
Tran203 <tshetshe203@gmail.com>
2023-08-11 10:25:17 +0200

    updated angular

--- a/app/src/app/app.component.ts
+++ a/app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = '';
+  surname = '';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const formData = { name: this.name, surname: this.surname };
+    this.http.post('http://localhost:3000/api/submit', formData).subscribe((response) => {
+      console.log('Data sent to server:', response);
+      // Clear the form fields after successful submission
+      this.name = '';
+      this.surname = '';
+    });
+  }
 }
--- a/app/src/app/app.module.ts
+++ a/app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Add FormsModule
+    HttpClientModule, // Add HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
